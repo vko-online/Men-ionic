978a276269b9f6ba4ab3e9b820cca39fc0cf0714
vko-online <m.diddy_11@mail.ru>
2015-10-09 16:08:04 +0600

    rm .idea folder

--- platforms/android/assets/www/modules/users/controllers/share.client.controller.js
+++
@@ -1,9 +0,0 @@
-// by bwin on 7/27/15.
-'use strict';
-
-angular.module('users').controller('ShareController', ['$scope', '$stateParams', '$http', '$location', 'Authentication', 'CORE_CONST',
-    function($scope, $stateParams, $http, $location, Authentication, CORE_CONST) {
-        $scope.authentication = Authentication;
-
-    }
-]);
